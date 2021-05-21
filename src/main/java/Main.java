00bcc19520eab52504b828f053700da8a1e7fef6
KiranjotKaur88 <kiranjotk24@gmail.com>
2021-05-20 20:59:58 -0400

    New changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Hello Vivek");
         System.out.println("Hello Jaspreet");
+        System.out.println("New changes");
+
     }
 
 }
